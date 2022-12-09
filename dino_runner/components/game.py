dadf98f0b134c918a237c999e4aa15c7b8d39365
DannyVargas <daniel.vargas.sosa@gmail.com>
2022-12-08 20:04:46 -0400

    Clase 4

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,9 +2,12 @@ import pygame
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.obstacles.shield import Shield
+from dino_runner.components.powerups.power_up_manager import PowerUpManager
 from dino_runner.components.score import Score
+from dino_runner.components import text_utils
 
-from dino_runner.utils.constants import BG, FONT_STYLE, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, SMALL_CACTUS, TITLE, FPS
+from dino_runner.utils.constants import BG, DEFAULT_TYPE, FONT_STYLE, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, SHIELD_TYPE, SMALL_CACTUS, TITLE, FPS
 
 class Game:
     def __init__(self):
@@ -23,6 +26,9 @@ class Game:
 
         self.death_count = 0
         self.score = Score()
+        self.power_up_manager = PowerUpManager()
+
+   
     
     def execute(self):
         self.executing = True
@@ -37,6 +43,9 @@ class Game:
         # Game loop: events - update - draw
         self.playing = True
         self.obstacle_manager.reset_obstacles()
+        self.score.reset()
+        self.game_speed = 20
+        self.power_up_manager.reset_power_ups()
         while self.playing:
             self.events()
             self.update()
@@ -53,6 +62,8 @@ class Game:
         self.player.update(user_input)
         self.obstacle_manager.update(self)
         self.score.update(self)
+        self.power_up_manager.update(self.game_speed, self.player, self.score.score)
+ 
 
 
     def draw(self):
@@ -62,7 +73,8 @@ class Game:
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         self.score.draw(self.screen)
-
+        self.power_up_manager.draw(self.screen)
+        self.draw_power_up_activate()
         pygame.display.update()
         pygame.display.flip()
 
@@ -83,16 +95,27 @@ class Game:
         half_screen_width = SCREEN_WIDTH // 2
 
         if self.death_count == 0:
-            font = pygame.font.Font(FONT_STYLE, 30)
-            text_component = font.render("Press any key to play", True, (0,0,0))
-            text_rect = text_component.get_rect()
-            text_rect.center = (half_screen_width, half_screen_height)
-            self.screen.blit(text_component,text_rect)
+
+            text, text_rect = text_utils.get_centered_message('Press any Key to Start')
+            self.screen.blit(text, text_rect)
+            #font = pygame.font.Font(FONT_STYLE, 30)
+            #text_component = font.render("Press any key to play", True, (0,0,0))
+            #text_rect = text_component.get_rect()
+            #text_rect.center = (half_screen_width, half_screen_height)
+            #self.screen.blit(text_component,text_rect)
         else:
+            text, text_rect = text_utils.get_centered_message('Press any Key to Restart')
+            score, score_rect = text_utils.get_centered_message('Your Score: ' + str(self.score.score),
+                                                                height=half_screen_height + 50)
+            
+            death, death_rect = text_utils.get_centered_message('Death count: ' + str(self.death_count),
+                                                                height=half_screen_height + 100)
+            self.screen.blit(score, score_rect)
+            self.screen.blit(text, text_rect)
+            self.screen.blit(death, death_rect)
             #Mostrar mensaje de volver a jugar ()
             # MOstrar total de muertes
             # MOstrar el puntaje
-            pass
         #Mostrar imagen en el menu*
 
         self.screen.blit(RUNNING[0], (half_screen_width - 30, half_screen_height + 140))
@@ -106,5 +129,23 @@ class Game:
             if event.type == pygame.QUIT:
                 self.executing = False
             elif event.type == pygame.KEYDOWN:
+
                 self.run()
+
+    def on_death (self):
+        is_invencible = self.player.type == SHIELD_TYPE
+        if not is_invencible:
+            self.playing = False
+            self.death_count += 1
+        return is_invencible
+
+    def draw_power_up_activate (self):
         
+        if self.player.has_power_up:
+            time_to_show = round ((self.player.power_up_time_up - pygame.time.get_ticks())) / 1000
+            if time_to_show >= 0:
+                pass
+                #pantalla
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
@@ -9,14 +10,22 @@ class ObstacleManager:
 
     def update (self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append (Cactus(SMALL_CACTUS))
+            if random.randint(0,1) == 0:
+                cactus_type = "SMALL"
+            else:
+                cactus_type = "LARGE"
+            self.obstacles.append (Cactus(cactus_type))
         
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(1000)
-                game.playing = False
-                break
+                if game.on_death():
+                    self.obstacles.remove(obstacle)
+                else:
+                #game.playing = False
+
+                    break
 
     def draw (self, screen):
         for obstacle in self.obstacles:
