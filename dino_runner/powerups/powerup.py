fc2141ba6763eb459dfe5b0e6509ff9a0a562153
Himinblatt <Robert.Villacorta@fundacion-jala.org>
2022-12-10 01:25:03 -0400

    Tarea 4

--- dino_runner/powerups/powerup.py
+++ dino_runner/powerups/powerup.py
@@ -4,22 +4,23 @@ from pygame.sprite import Sprite
 from dino_runner.utils.constants import SCREEN_WIDTH
 
 class PowerUp(Sprite):
-    def __init__(self,image, type):
-        self.type = type
+    def __init__(self, image, type):
         self.image = image
+        self.type = type
         self.rect = self.image.get_rect()
-        self.rect.x = SCREEN_WIDTH + randint (800, 1000)
-        self.rect.y = randint (125, 185)
-
+        self.rect.x = SCREEN_WIDTH + randint(800,1000)
+        self.rect.y = randint(125, 300)
         self.start_time = 0
-        self.duration = randint (5, 7)
+        self.duration = (5)
+        
 
 
-    def update (self, game_speed, power_ups):
+
+    def update(self, game_speed, power_ups):
         self.rect.x -= game_speed
         if self.rect.x < -self.rect.width:
             power_ups.pop()
 
-    def draw (self, screen):
-        screen.blit(self.image, (self.rect.x,self.rect.y))
-    
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
